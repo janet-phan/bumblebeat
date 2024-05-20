13c7af8a6b2741dbf3193dd4536f20d1a6ff03dd
Janet <janet_phan@outlook.com>
2024-05-19 23:10:58 -0400

    added css into app.js html

--- app.js
+++ app.js
@@ -53,7 +53,265 @@ app.get("/", (request, response, next) => {
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>BumbleBeat</title>
-      <link rel="stylesheet" href="./public/styles/style.css" />
+      // <link rel="stylesheet" href="./public/styles/style.css" />
+      <style>
+      :root {
+        --black: #2E2C2C;
+        --darkblack: #1a1919;
+        --yellow: #FFDE59;
+        --gold: #F8A70E;
+        --brown: #9B6941;
+        --white: #F6F6E4;
+        --lightgray: #7B7B7B;
+    }
+    
+    .fredoka-normal {
+        font-family: "Fredoka", sans-serif;
+        font-optical-sizing: auto;
+        font-weight: 100;
+        font-style: normal;
+        font-variation-settings:
+          "wdth" 100;
+      }
+    
+      body, html {
+        height: 100%;
+        min-height: 100vh;
+        margin: 0;
+        padding: 0;
+      }
+    
+    body {
+        background: linear-gradient(180deg, var(--gold) 0%, var(--black) 55%);
+        color: var(--white);
+        font-family: "Fredoka", sans-serif;
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+    }
+    
+    body a {
+        color: var(--white);
+        text-decoration: none;
+    }
+    
+    nav ul {
+        display: flex;
+        justify-content: space-around;
+    }
+    
+    nav ul li {
+        text-decoration: none;
+        padding: 0 20px;
+        margin-bottom: 2em;
+        list-style-type: none;
+    }
+    
+    nav input {
+      width: 100%;
+      padding: .5rem;
+      border-radius: 20px;
+    }
+    
+    footer {
+        display: flex;
+        justify-content: space-around;
+        margin: 2em auto;
+    }
+    
+    footer img {
+      width: 200px;
+      filter: drop-shadow(0 0 1.5rem var(--white));
+    }
+    
+    main {
+        width: 85%;
+        margin: auto;
+    }
+    
+    .row {
+        display: flex;
+        flex-wrap: wrap;
+        width: 100%;
+        margin-top: 2em;
+    } 
+    
+    .column1 {
+        display: flex;
+        flex-direction: column;
+        flex-basis: 100%;
+        flex: 2;     
+    }
+    
+    .column2 {
+        display: flex;
+        flex-direction: column;
+        flex-basis: 100%;
+        flex: 3;     
+    }
+    
+    /* circular track playing */
+    .currently-playing {
+        border-radius: 50%;
+        width: 20em;
+        height: 20em;
+        background-color: var(--yellow);
+        box-shadow: 0 0 10px var(--yellow);
+        position: absolute;
+    }
+    
+    .current-artist {
+        text-transform: lowercase;
+        margin-top: 1em;
+    }
+    
+    .current-artist h1 {
+        color: var(--white);
+        text-decoration: none;
+        font-size: 2em;
+        font-weight: 400;
+        margin-bottom: 0;
+    }
+    
+    .current-title h2 {
+        color: var(--black);
+        font-size: 1.5em;
+        font-weight: 200;
+        margin-top: 0;
+    }
+    
+    i {
+        margin-left: 0.45em;
+        font-size: .75em;
+    }
+    
+    .time {
+        text-align: center;
+        width: 20em;
+        margin-top: 1em;
+    }
+    
+    /* borrowed code from codepen for this rounded bar */
+    .progress{
+        /* margin: 100px auto; */
+        /* text-align: center; */
+        width: 45%;
+        /* background: white; */
+        transform: rotate(180deg);
+        position: relative;
+        margin-top: 10em;
+        padding-left: 25em;
+      }
+      .barOverflow{ /* Wraps the rotating .bar */
+        position: relative;
+        overflow: hidden;
+        width: 25em; height: 12em; /* Half circle (overflow) */
+        /* background: blue; */
+      }
+      .bar{
+        /* position: absolute; */
+        margin: auto;
+        width: 24em; height: 24em; 
+        border-radius: 50%;
+        box-sizing: border-box;
+        border: 18px solid #FEBE6E;     
+        border-left-color: #9B6941;
+        border-bottom-color: #9B6941;
+        transform: rotate(5deg);  /* (per * 1.8) - 45 */
+      /* Exa: 50% - (50 * 1.8) - 45 = 45deg */
+      /* background: pink; */
+      }
+      
+      .playing-song-title {
+        font-size: 3em;
+      }
+      
+      .soundwave {
+        height: 10em;
+        background-image: url(../images/yellow-soundwave.gif);
+        background-size: contain;
+        background-repeat: no-repeat;
+        margin-left: -5em;
+      }
+    
+      /* volume, playback, track share/repeat/shuffle/add to list icons */
+      .playback-row {
+        display: flex;
+        flex-wrap: wrap;
+        width: 100%;
+        margin-top: 2em;
+        justify-content: center;
+        align-items: center;
+    } 
+    
+      .volume-settings {
+        display: flex;
+        flex-wrap: wrap;
+        flex: 1;
+        margin-top: 2em;
+        justify-content: center;
+      }
+    
+       /* CSS for the volume bar */
+       .volume-bar {
+        width: 150px;
+        height: 20px;
+        background-color: var(--gold);
+        border-radius: 10px;
+        overflow: hidden;
+      }
+    
+      .volume-level {
+        height: 100%;
+        background-color: var(--yellow);
+      }
+    
+      /* CSS for the volume control slidey ball thingy */
+      .volume-control {
+        width: 150px;
+        height: 40px;
+        margin-left: -150px;
+      }
+    
+      input[type="range"] {
+        width: 100%;
+        -webkit-appearance: none;
+        background-color: transparent;
+        margin: 0;
+      }
+    
+      input[type="range"]::-webkit-slider-thumb {
+        -webkit-appearance: none;
+        height: 20px;
+        width: 20px;
+        border-radius: 50%;
+        background-color: var(--gold);
+      }
+    
+      .playback-settings {
+        font-size: 5em;
+        flex: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+      }
+    
+      #gold {
+        color: var(--gold);
+        font-size: 1.5em;
+      }
+    
+      .track-setting {
+        flex: 1;
+        display: flex;
+        justify-content: center;
+        font-size: 2em;
+      }
+    
+      .track-setting i {
+        margin: .5em;
+      }
+    
+      </style>
       <script
         src="https://kit.fontawesome.com/012a2bcae9.js"
         crossorigin="anonymous"></script>
